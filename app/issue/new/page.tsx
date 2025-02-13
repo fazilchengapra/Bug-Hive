e03c489c8337f5a58a15fcb8b2cf1737b9a1aa3b
Fazil Chengapra <muhammedfazilchengapra@gmail.com>
2025-02-13 16:40:24 +0530

    handle form submission

--- app/issue/new/page.tsx
+++ app/issue/new/page.tsx
@@ -1,16 +1,31 @@
+'use client'
 import { Button, TextArea, TextField } from "@radix-ui/themes";
-import React from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import {useForm} from 'react-hook-form'
+
+
+interface issueForm {
+  title: string;
+  description: string;
+}
 
 const NewIssuePage = () => {
+  const route = useRouter()
+  const {register, handleSubmit} =useForm<issueForm>()
   return (
     <div>
-      <div className="max-w-xl space-y-2">
-        <TextField.Root placeholder="Title">
-          <TextField.Slot></TextField.Slot>
-        </TextField.Root>
-        <TextArea placeholder="Description"></TextArea>
+      <form className="max-w-xl space-y-2" onSubmit={handleSubmit(async(data) => {
+        const result = await axios.post('/api/issues', data)
+        console.log(result)
+        route.push('/issue')
+      })}>
+        <TextField.Root {...register('title')}
+          placeholder="Title"
+        ></TextField.Root>
+        <TextArea placeholder="Description" {...register('description')} />
         <Button>Submit</Button>
-      </div>
+      </form>
     </div>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import "@radix-ui/themes/styles.css";
-import '@/app/theme-config.css'
+import "@/app/theme-config.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -31,9 +31,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Theme>
+        <Theme accentColor="purple">
           <Navbar />
           <main className="p-5">{children}</main>
+          
         </Theme>
       </body>
     </html>
