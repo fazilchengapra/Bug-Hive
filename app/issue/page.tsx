b3ece870e9ef979a86a2b6fd37b41cfd9bc1d38b
Fazil Chengapra <muhammedfazilchengapra@gmail.com>
2025-02-13 13:41:52 +0530

    setup Radix UI

--- app/issue/page.tsx
+++ app/issue/page.tsx
@@ -1,8 +1,13 @@
+import { Button } from '@radix-ui/themes'
 import React from 'react'
 
 const Issue = () => {
   return (
-    <div>Issue Page</div>
+    <div className=''>
+        <div className='ml-2'>
+        <Button>New issue</Button>
+        </div>
+    </div>
   )
 }
 
