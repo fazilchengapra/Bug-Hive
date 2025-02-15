b62d04df0e921ee51154426d71818e6221c7f35f
Fazil Chengapra <muhammedfazilchengapra@gmail.com>
2025-02-15 16:03:30 +0530

    Added dialog alert

--- app/issue/[id]/IssueDeleteButton.tsx
+++ app/issue/[id]/IssueDeleteButton.tsx
@@ -1,8 +1,28 @@
-import { Button } from "@radix-ui/themes";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import React from "react";
 
-const IssueDeleteButton = ({issueId}:{issueId:number}) => {
-  return <Button color="red">Delete</Button>;
+const IssueDeleteButton = ({ issueId }: { issueId: number }) => {
+  return (
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">Delete</Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Are You Sure Want to Delete</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are You Sure This Action Cannot Provide Undo Action
+        </AlertDialog.Description>
+        <Flex  mt={'4'} gap={'5'} justify={'end'}>
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">Cancel</Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button variant="solid" color="red">Delete</Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
+  );
 };
 
 export default IssueDeleteButton;
