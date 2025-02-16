b08b5187a4919d2d76372016c86ad313c08d5acd
Fazil Chengapra <muhammedfazilchengapra@gmail.com>
2025-02-16 17:35:32 +0530

    show user in assign assign select

--- app/issue/[id]/AssignSelect.tsx
+++ app/issue/[id]/AssignSelect.tsx
@@ -1,14 +1,20 @@
+import { prisma } from "@/prisma/client";
 import { Select } from "@radix-ui/themes";
 import React from "react";
 
-const AssignSelect = () => {
+const AssignSelect = async() => {
+  const users = await prisma.user.findMany({
+    orderBy:{
+      name: 'asc'
+    }
+  })
   return (
     <Select.Root >
       <Select.Trigger placeholder="Assign..."/>
       <Select.Content>
         <Select.Group>
           <Select.Label>Users</Select.Label>
-          <Select.Item value="1">Fazil Chengapra</Select.Item>
+          {users.map(user => <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>)}
         </Select.Group>
       </Select.Content>
     </Select.Root>
