3c70d5a44155f9e7ccf3cf753389db190afd6b0e
Fazil Chengapra <muhammedfazilchengapra@gmail.com>
2025-02-17 08:36:44 +0530

    fetch data with react query

--- app/issue/[id]/AssignSelect.tsx
+++ app/issue/[id]/AssignSelect.tsx
@@ -1,20 +1,35 @@
-import { prisma } from "@/prisma/client";
+"use client";
+import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import React from "react";
+import { Skeleton } from "@/app/components";
 
-const AssignSelect = async() => {
-  const users = await prisma.user.findMany({
-    orderBy:{
-      name: 'asc'
-    }
-  })
+const AssignSelect = ({ issueId }: { issueId: string }) => {
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    staleTime: 60 * 1000,
+    retry: 3,
+  });
+  if (isLoading) return <Skeleton />;
+  if (error) return null;
   return (
-    <Select.Root >
-      <Select.Trigger placeholder="Assign..."/>
+    <Select.Root>
+      <Select.Trigger placeholder="Assign..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Users</Select.Label>
-          {users.map(user => <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>)}
+          {users?.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
+          ))}
         </Select.Group>
       </Select.Content>
     </Select.Root>
