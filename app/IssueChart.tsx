2b91b2433d15e7aab426ad3a61a7c10131e53898
Fazil Chengapra <muhammedfazilchengapra@gmail.com>
2025-02-18 13:03:45 +0530

    build the issue chart component

---
+++ app/IssueChart.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { Card } from "@radix-ui/themes";
+import React from "react";
+import { ResponsiveContainer, BarChart, XAxis, YAxis, Bar } from "recharts";
+
+interface Props {
+  open: number;
+  in_progress: number;
+  closed: number;
+}
+
+const IssueChart = ({ open, in_progress, closed }: Props) => {
+  const data = [
+    { label: "Open", value: open },
+    { label: "In Progress", value: in_progress },
+    { label: "Closed", value: closed },
+  ];
+
+  return (
+    <Card>
+      <ResponsiveContainer width="100%" height={200}>
+        <BarChart data={data}>
+          <XAxis dataKey="label" />
+          <YAxis />
+          <Bar dataKey="value" fill="#8884d8" barSize={100}/> {/* Added fill color */}
+        </BarChart>
+      </ResponsiveContainer>
+    </Card>
+  );
+};
+
+export default IssueChart;
--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,27 @@
 import { prisma } from "@/prisma/client";
-import IssueSummary from "./IssueSummary";
-
+import IssueChart from "./IssueChart";
 
 export default async function Home() {
-  const open =await prisma.issue.count({
-    where:{
-      status: "OPEN"
-    }
-  })
+  const open = await prisma.issue.count({
+    where: {
+      status: "OPEN",
+    },
+  });
 
-  const closed =await prisma.issue.count({
-    where:{
-      status: "CLOSED"
-    }
-  })
+  const closed = await prisma.issue.count({
+    where: {
+      status: "CLOSED",
+    },
+  });
 
-  const in_progress =await prisma.issue.count({
-    where:{
-      status: "IN_PROGRESS"
-    }
-  })
+  const in_progress = await prisma.issue.count({
+    where: {
+      status: "IN_PROGRESS",
+    },
+  });
   return (
     <>
-      <IssueSummary closed={closed} open={open} in_progress={in_progress}/>
+      <IssueChart closed={closed} open={open} in_progress={in_progress} />
     </>
   );
 }
