6bb0b37ed639c4953943d475a9c7940d24ed05ef
Fazil Chengapra <muhammedfazilchengapra@gmail.com>
2025-02-24 10:15:16 +0530

    github auth added

--- app/auth/authOptions.ts
+++ app/auth/authOptions.ts
@@ -1,19 +1,25 @@
-import { prisma } from "@/prisma/client"
-import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "@/prisma/client";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
 
-const auth:NextAuthOptions = {
-    adapter: PrismaAdapter(prisma),
-    providers: [
-      GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID!,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      }),
-    ],
-    session:{
-      strategy: 'jwt'
-    }
-  }
+const auth: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+      allowDangerousEmailAccountLinking:true
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+};
 
-export default auth
+export default auth;
